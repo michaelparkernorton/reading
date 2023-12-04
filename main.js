3f9d306ecf68aa1d1512e3cb7906ca3153a0319b
Mike <michaelparkernorton.va@gmail.com>
2023-12-04 10:56:54 -0500

    updates

--- main.js
+++ main.js
@@ -1,38 +1,39 @@
-window.addEventListener('visibilitychange', () =>{
-    if (document.visibilityState === 'visible') window.location.reload();
-    else console.log('user leaves the page')
-})
-const textarea = document.querySelector('[data-textarea]');
-const startButton = document.querySelector('[data-button]');
+window.addEventListener('click', function () {
+  window.location.reload();
+});
+
+window.addEventListener('visibilitychange', () => {
+  if (document.visibilityState === 'visible') window.location.reload();
+  else console.log('user leaves the page');
+});
+// const textarea = document.querySelector('[data-textarea]');
+// const startButton = document.querySelector('[data-button]');
 let text;
 navigator.clipboard
-.readText()
-.then((clipText) => (textarea.textContent = clipText))
-.then(value => {
-  text = value
-  console.log(text);
-});
+  .readText()
+  // .then((clipText) => (textarea.textContent = clipText))
+  .then((value) => {
+    text = value;
+    console.log(text);
+  });
 
 let array;
 const body = document.querySelector('[data-body]');
 const header = document.querySelector('[data-header]');
-const textareaContainer = document.querySelector('.textarea');
-startButton.addEventListener('click', generate);
+// const textareaContainer = document.querySelector('.textarea');
+// startButton.addEventListener('click', generate);
 
-document.body.onkeydown = function(e) {
-  if (e.key == " " ||
-      e.code == "Space" ||      
-      e.keyCode == 32      
-  ) {
-    generate();
+document.body.onkeydown = function (e) {
+  if (e.key == ' ' || e.code == 'Space' || e.keyCode == 32) {
+    document.body.requestFullscreen();
   }
-}
+};
 
 let count = 0;
 let time = 0;
 
 const fast = 50;
-const slow = 1000;
+const slow = 900;
 let pace;
 let speed = fast;
 let mode = 'dark';
@@ -41,13 +42,16 @@ let myInterval;
 if (mode == 'dark') {
   body.style.backgroundColor = 'black';
 }
-textarea.style.display = 'none';
-textareaContainer.style.display = 'none';
+// textarea.style.display = 'none';
+// textareaContainer.style.display = 'none';
+
+// generate();
+
+setTimeout(generate, 5);
 
 function generate() {
-  document.body.requestFullscreen();
   // text = textarea.value;
-  startButton.style.display = 'none';
+  // startButton.style.display = 'none';
   header.style.display = 'block';
   header.style.color = 'white';
   if (text == '') {
@@ -60,6 +64,8 @@ function generate() {
   myInterval = setInterval(myTimer, slow);
 }
 
+let number = 0;
+let hex;
 function myTimer() {
   header.textContent = array[count];
   if (array[count] < 5) {
@@ -87,9 +93,12 @@ function myTimer() {
     myInterval = setInterval(myTimer, pace * speed);
   }
   if (mode == 'light') {
-    header.style.color = '#' + count / 4;
+    header.style.color = '#000000';
   } else if (mode == 'dark') {
-    header.style.color = '#' + (999999 - count / 4);
+    number = 11393254 + count / 1000;
+    hex = number.toString(16);
+    console.log(hex);
+    header.style.color = '#' + hex;
     body.style.backgroundColor = 'black';
   }
   count++;
@@ -100,10 +109,10 @@ function myTimer() {
   }
 }
 
-function exitProgram () {
+function exitProgram() {
   document.exitFullscreen();
   // textarea.style.display = 'block';
-  startButton.style.display = 'flex';
+  // startButton.style.display = 'flex';
   // textareaContainer.style.display = 'flex';
   header.style.display = 'none';
 }
